15e174fe1bdeaffe08826824d69f5d6886b2217f
Ameer Hamza <142672783+AmeerHamza6758@users.noreply.github.com>
2025-04-18 19:42:47 +0500

    DOM

--- Basics/JS/index.js
+++ Basics/JS/index.js
@@ -160,12 +160,15 @@ console.log(calculatedUserAge, "The calculated Age...");
 // for
 // for-in (Object)
 // for-of (Array)
+//Break
+//Continue
 
 // while (conditon) {
 //statements
 //increment
 //decrement
 // }
+let i = 1;
 let num = 5;
 let itteration = 10;
 
@@ -182,6 +185,66 @@ let itteration = 10;
 // } while (i <= itteration);
 
 
-for(let i=1;i<=itteration;i++){
-    console.log(`${num} * ${i} =${num * i}`);
+// for (let i = 1; i <= itteration; i++) {
+//     console.log(`${num} * ${i} =${num * i}`);
+// }
+
+// For-in-Loop (Object)
+
+const student = {
+    // key:value
+    name: "Ameer",
+    email: "student@yopmail.com",
+    phone: 9847466454,
+    rollNo: 323,
+    age: 20,
+    gender: "Male"
 }
+
+for (const key in student) {
+    // console.log(key,'These are keys');
+    // console.log(`These are keys ${key} and values are ${student[key]}`);
+}
+
+
+// For-Of-Loop
+
+const dressPrices = [2000, 1500, 3000, 4000, 5000, 4500, 3500]
+let afterDiscount = []
+for (const item of dressPrices) {
+    const dis = 0.25 * item
+    const afterDisc = item - dis
+    afterDiscount.push(afterDisc)
+}
+// console.log('Hey discounts are here: ', afterDiscount);
+
+
+// Break and Continue Statements
+let start = 1;
+let end = 10;
+
+// while (start <= end) {
+//     start++
+//     if (start === 5) {
+//         continue;
+//     }
+//     console.log('Continue statement', start);
+// }
+
+// for (let start = 1; start <= 10; start++) {
+//     if (start === 5) {
+//         continue;
+//     }
+//     console.log('Break', start);
+// }
+
+
+
+// DOM Manipulation
+const documentData = document.getElementById("heading")
+console.log('Document', documentData.textContent)
+
+// document.title ='Hello'
+documentData.innerText="Ameer Hamza"
+
+// https://www.freecodecamp.org/news/what-is-the-dom-document-object-model-meaning-in-javascript/
